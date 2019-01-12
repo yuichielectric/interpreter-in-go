e839cf8ade4e72ca1f300d0694560f3f7e264531
Yuichi Tanaka <yuichielectric@github.com>
2019-01-12 20:56:43 +0900

    Implement first edition

---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier + Literal
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123456
+
+	// Operations
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiter
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
